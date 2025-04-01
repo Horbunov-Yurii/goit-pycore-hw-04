bb9fa810fbc94f1ad794dd7130cc3b989bd136b3
Horbunov Yurii <urijnet@gmail.com>
2025-04-01 12:29:45 +0300

    fix

--- task-2.py
+++ task-2.py
@@ -3,8 +3,9 @@ def get_cats_info(path: str) -> list:
     try:
         with open(path, encoding='utf-8') as file:
             for line in file:
+               
                 parts = line.strip().split(',')
-            
+               
                 if len(parts) == 3:
                     cat_id, name, age = parts
                     cats_list.append({"id": cat_id, "name": name, "age": age})
